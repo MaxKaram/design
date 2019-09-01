5a69bdbf3789b9df052059ea0e68fcdd2057e75e
Alexander <a.morgoth.b@gmail.com>
2019-09-01 17:15:21 +0300

    build: scripts for mobile menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,26 @@
-//=require ...
+[ 'resize', 'DOMContentLoaded' ].forEach(function(e) {
+	window.addEventListener(e, () => {
+		let menuIcon = document.querySelector('.icon-menu-button');
+		let nav = document.querySelector('.nav');
 
-let arr = [1, 2, 3];
+		if (window.matchMedia(`(max-width: ${nav.dataset.menuBreakpoint}px)`).matches) {
+			addMobileMenu();
+		} else {
+			removeMobileMenu();
+		}
 
-console.log(...arr)
+		function addMobileMenu() {
+			menuIcon.classList.add('icon-menu-button_visible');
+			nav.classList.add('nav_mobile');
+
+			menuIcon.onclick = () => {
+				nav.classList.toggle('nav_mobile_open');
+			};
+		}
+
+		function removeMobileMenu() {
+			menuIcon.classList.remove('icon-menu-button_visible');
+			nav.classList.remove('nav_mobile');
+		}
+	});
+});
