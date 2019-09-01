a03e52ad002bfca1be3f71b2f33f972edc24e10e
Alexander <a.morgoth.b@gmail.com>
2019-09-01 17:10:06 +0300

    build: mobile menu

--- dest/js/script.js
+++ dest/js/script.js
@@ -1,8 +1,28 @@
 "use strict";
 
-var _console;
+['resize', 'DOMContentLoaded'].forEach(function (e) {
+  window.addEventListener(e, function () {
+    var menuIcon = document.querySelector('.icon-menu-button');
+    var nav = document.querySelector('.nav');
 
-//=require ...
-var arr = [1, 2, 3];
+    if (window.matchMedia("(max-width: ".concat(nav.dataset.menuBreakpoint, "px)")).matches) {
+      addMobileMenu();
+    } else {
+      removeMobileMenu();
+    }
 
-(_console = console).log.apply(_console, arr);
+    function addMobileMenu() {
+      menuIcon.classList.add('icon-menu-button_visible');
+      nav.classList.add('nav_mobile');
+
+      menuIcon.onclick = function () {
+        nav.classList.toggle('nav_mobile_open');
+      };
+    }
+
+    function removeMobileMenu() {
+      menuIcon.classList.remove('icon-menu-button_visible');
+      nav.classList.remove('nav_mobile');
+    }
+  });
+});
